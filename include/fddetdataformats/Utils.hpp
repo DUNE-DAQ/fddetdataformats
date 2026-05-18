34c20d70604d64e8315104411556b6b12d2bb75d
Your Name <you@example.com>
2026-05-18 16:12:21 -0500

    JCF: DUNE-DAQ/daq-deliverables#215: finish the generic get_adc and set_adc functions and flesh out their unit tests
    Specifically, I repurpose Ron's work from #48 to upgrade get_adc and
    set_adc to handle ADC values expressed in numbers of bits which differ
    from the number of bits of the type underlying them
    
    Also, range checking is introduced so get_adc and set_adc throw if
    they're passed a bad index
    
    Finally, unit tests are added which test all of this

--- include/fddetdataformats/Utils.hpp
+++ include/fddetdataformats/Utils.hpp
@@ -9,18 +9,103 @@
 #ifndef FDDETDATAFORMATS_INCLUDE_FDDETDATAFORMATS_UTILS_HPP_
 #define FDDETDATAFORMATS_INCLUDE_FDDETDATAFORMATS_UTILS_HPP_
 
+#include <cassert>
+#include <format>
+#include <stdexcept> // Provides std::out_of_range
+
 namespace dunedaq::fddetdataformats {
-
-  template<typename WordType, int Rows, int Columns>
-  void set_adc(const int i_channel, const int i_adc, WordType adc_val, WordType (&adc_arr)[Rows][Columns]) {
-    adc_arr[i_channel][i_adc] = adc_val;
-  }
-
-  template<typename WordType, int Rows, int Columns>
-  WordType get_adc(const int i_channel, const int i_adc, const WordType (&adc_arr)[Rows][Columns]) {
-    return adc_arr[i_channel][i_adc];
-  }
   
+  // Rows and Columns refer to physical rows and columns of WordType
+  // in the adc_matrix object; i_channel and i_adc refer to logical
+  // rows and columns of ADC values in the event
+  
+  template<typename WordType, int Rows, int Columns, int BitsPerADC>
+  WordType get_adc(const int i_channel, const int i_adc, const WordType (&adc_matrix)[Rows][Columns]) {
+
+    static_assert(std::is_integral_v<WordType> &&
+		  std::is_unsigned_v<WordType>,
+		  "WordType must be an unsigned integral type");
+    constexpr int bits_per_word = std::numeric_limits<WordType>::digits;
+    constexpr int max_adc_index = Columns * bits_per_word / BitsPerADC - 1;
+    
+    if (i_channel < 0 || i_channel >= Rows) {
+      throw std::out_of_range(std::format("Requested channel index of {} is outside of allowed range 0-{}", i_channel, Rows - 1));
+    }
+
+    if (i_adc < 0 || i_adc > max_adc_index) {
+      throw std::out_of_range(std::format("Requested adc index of {} is outside of allowed range 0-{}", i_adc, max_adc_index));
+    }
+
+    // The index of the first (and sometimes only) word containing the required ADC value
+    int i_word = BitsPerADC * i_adc / bits_per_word;
+    assert(i_word < Columns);
+
+    // Where in the word the lowest bit of our ADC value is located
+    int first_bit_position = (BitsPerADC * i_adc) % bits_per_word;
+
+    // How many bits of our desired ADC are located in the `i_word`th word
+    int bits_from_first_word = std::min(BitsPerADC, bits_per_word - first_bit_position);
+
+    WordType adc = adc_matrix[i_channel][i_word] >> first_bit_position; // NOLINT(build/unsigned)
+
+    // If we didn't get the full BitsPerADC bits from this word, we need the rest from the next word
+    if (bits_from_first_word < BitsPerADC) {
+      assert(i_word < Columns - 1);
+      adc |= adc_matrix[i_channel][i_word + 1] << bits_from_first_word;
+    }
+    
+    // Mask out all but the lowest BitsPerADC bits;
+    return adc & ((static_cast<WordType>(1) << BitsPerADC) - 1);
+  }
+
+  // Rows and Columns refer to physical rows and columns of WordType
+  // in the adc_matrix object; i_channel and i_adc refer to logical
+  // rows and columns of ADC values in the event
+
+  template<typename WordType, int Rows, int Columns, int BitsPerADC>
+  void set_adc(const int i_channel, const int i_adc, WordType adc_val, WordType (&adc_matrix)[Rows][Columns]) {
+    static_assert(std::is_integral_v<WordType> &&
+		  std::is_unsigned_v<WordType>,
+		  "WordType must be an unsigned integral type");
+
+    constexpr int bits_per_word = std::numeric_limits<WordType>::digits;
+    constexpr int max_adc_index = Columns * bits_per_word / BitsPerADC - 1;
+
+    if (i_channel < 0 || i_channel >= Rows) {
+      throw std::out_of_range(std::format("Requested channel index of {} is outside of allowed range 0-{}", i_channel, Rows - 1));
+    }
+
+    if (i_adc < 0 || i_adc > max_adc_index) {
+      throw std::out_of_range(std::format("Requested adc index of {} is outside of allowed range 0-{}", i_adc, max_adc_index));
+    }
+
+    if (BitsPerADC < bits_per_word && adc_val >= static_cast<WordType>(1) << BitsPerADC) {
+      throw std::out_of_range(std::format("Requested adc value of {} exceeds the maximum of {}", adc_val, (static_cast<WordType>(1) << BitsPerADC) - 1));
+    }
+
+    // The index of the first (and sometimes only) word containing the required ADC value
+    int i_word = BitsPerADC * i_adc / bits_per_word;
+    assert(i_word < Columns);
+
+    // Where in the word the lowest bit of our ADC value is located
+    int first_bit_position = (BitsPerADC * i_adc) % bits_per_word;
+
+    // How many bits of our desired ADC are located in the `i_word`th word
+    int bits_from_first_word = std::min(BitsPerADC, bits_per_word - first_bit_position);
+
+    // Create a mask that covers exactly the bits we want to modify (bits_from_first_word bits starting at first_bit_position)
+    WordType adc_mask = ((static_cast<WordType>(1) << bits_from_first_word) - 1) << first_bit_position;
+    adc_matrix[i_channel][i_word] = (adc_matrix[i_channel][i_word] & ~adc_mask) | ((static_cast<WordType>(adc_val) << first_bit_position) & adc_mask);
+
+    // If we didn't put the full BitsPerADC bits in this word, we need to put the rest in the next word                        
+    if (bits_from_first_word < BitsPerADC) {
+      assert(i_word < Columns - 1);
+      int bits_from_second_word = BitsPerADC - bits_from_first_word;
+      WordType adc_mask2 = (static_cast<WordType>(1) << bits_from_second_word) - 1;
+      adc_matrix[i_channel][i_word + 1] = (adc_matrix[i_channel][i_word + 1] & ~adc_mask2) | ((adc_val >> bits_from_first_word) & adc_mask2);
+    }
+  }
+
 } // namespace dunedaq::fddetdataformats
 
 #endif // FDDETDATAFORMATS_INCLUDE_FDDETDATAFORMATS_UTILS_HPP_
--- unittest/Utils_test.cxx
+++ unittest/Utils_test.cxx
@@ -8,8 +8,6 @@
 
 #include "fddetdataformats/Utils.hpp"
 
-#include "UnitTestUtils.hpp"
-
 #define BOOST_TEST_MODULE Utils_test
 
 #include "boost/test/unit_test.hpp"
@@ -18,34 +16,89 @@
 
 BOOST_AUTO_TEST_SUITE(Utils_test)
 
-BOOST_AUTO_TEST_CASE(Utils_SanityCheck)
+BOOST_AUTO_TEST_CASE(Utils_ADCOperation_SanityChecks)
 {
-  using namespace dunedaq::fddetdataformats;
-
+  using wordtype_t = uint32_t;
+  constexpr int bits_per_word = std::numeric_limits<wordtype_t>::digits;
+  
   constexpr int nrows = 2;
   constexpr int ncols = 3;
-  uint32_t myarr[nrows][ncols] = {{1,2,3}, {1001, 1002, 1003}};
+  wordtype_t myarr[nrows][ncols] = {{1,2,3}, {1001, 1002, 1003}};
 
-  set_adc<uint32_t, nrows, ncols>(0, 1, 4000, myarr);
-
-  for (auto i = 0; i < nrows; ++i)
-    for (auto j = 0; j < ncols; ++j)
-      std::cout << myarr[i][j] << "\n";
-
-  auto generated_adcs = generate_matrix<uint32_t>(nrows, ncols, 2000);
-  for (size_t i_r = 0; i_r < generated_adcs.size(); ++i_r) {
-    for (size_t i_c = 0; i_c < generated_adcs[i_r].size(); ++i_c) {
-      std::cout << generated_adcs[i_r][i_c] << " ";
-      set_adc<uint32_t, nrows, ncols>(i_r, i_c, generated_adcs[i_r][i_c], myarr);
+  // First, test get_adc if the ADC representation aligns with the C++ type
+  for (int i_r = 0; i_r < nrows; ++i_r) {
+    for (int i_c = 0; i_c < ncols; ++i_c) {
+      BOOST_REQUIRE_EQUAL(myarr[i_r][i_c], (dunedaq::fddetdataformats::get_adc<wordtype_t, nrows, ncols, bits_per_word>(i_r, i_c, myarr)));
     }
-    std::cout << "\n";
   }
+
+  // Next, test set_adc if the ADC representation aligns with the C++ type
+
+  constexpr wordtype_t inputarr[nrows][ncols] = {{1234, 5678, 9101}, {1121, 3141, 5161}};
+  for (int i_r = 0; i_r < nrows; ++i_r) {
+    for (int i_c = 0; i_c < ncols; ++i_c) {
+      dunedaq::fddetdataformats::set_adc<wordtype_t, nrows, ncols, bits_per_word>(i_r, i_c, inputarr[i_r][i_c], myarr);
+    }
+  }
+
+  for (int i_r = 0; i_r < nrows; ++i_r) {
+    for (int i_c = 0; i_c < ncols; ++i_c) {
+      BOOST_REQUIRE_EQUAL(inputarr[i_r][i_c], (dunedaq::fddetdataformats::get_adc<wordtype_t, nrows, ncols, bits_per_word>(i_r, i_c, myarr)));
+    }
+  }
+
+  // Now let's see what happens when ADC representation doesn't align with the C++ type
+
+  constexpr int bits_per_adc = 6;
+  static_assert(bits_per_word % bits_per_adc != 0);
+  static_assert((bits_per_word * ncols) % bits_per_adc == 0);
+
+  constexpr int nchannels = nrows;
+  constexpr int nadcs = bits_per_word * ncols / bits_per_adc;
+
+  constexpr wordtype_t inputarr2[nchannels][nadcs] = {
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+    {63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48}
+  };
+
+  for (int i_ch = 0; i_ch < nchannels; ++i_ch) {
+    for (int i_adc = 0; i_adc < nadcs; ++i_adc) {
+      dunedaq::fddetdataformats::set_adc<wordtype_t, nrows, ncols, bits_per_adc>(i_ch, i_adc, inputarr2[i_ch][i_adc], myarr);
+    }
+  }
+
+  for (int i_ch = 0; i_ch < nchannels; ++i_ch) {
+    for (int i_adc = 0; i_adc < nadcs; ++i_adc) {
+      BOOST_REQUIRE_EQUAL(inputarr2[i_ch][i_adc], (dunedaq::fddetdataformats::get_adc<wordtype_t, nrows, ncols, bits_per_adc>(i_ch, i_adc, myarr)));      
+    }
+  }
+
+  // Note that in the range checks below, 0 is an obviously good index and -1 is an obviously bad index
   
-  for (size_t i_r = 0; i_r < generated_adcs.size(); ++i_r) {
-    for (size_t i_c = 0; i_c < generated_adcs[i_r].size(); ++i_c) {
-      BOOST_REQUIRE_EQUAL(generated_adcs[i_r][i_c], myarr[i_r][i_c]);
-    }
-  }
+  constexpr int bad_adc_index = ncols * bits_per_word / bits_per_adc;
+  constexpr wordtype_t good_adc_val = 1;
+  constexpr wordtype_t bad_adc_val = static_cast<wordtype_t>(1) << bits_per_adc;
+  
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::get_adc<wordtype_t, nrows, ncols, bits_per_adc>(-1, 0, myarr)),
+		    std::out_of_range);
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::get_adc<wordtype_t, nrows, ncols, bits_per_adc>(nrows, 0, myarr)),
+		    std::out_of_range);
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::get_adc<wordtype_t, nrows, ncols, bits_per_adc>(0, -1, myarr)),
+		    std::out_of_range);
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::get_adc<wordtype_t, nrows, ncols, bits_per_adc>(0, bad_adc_index, myarr)),
+		    std::out_of_range);
+
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::set_adc<wordtype_t, nrows, ncols, bits_per_adc>(-1, 0, good_adc_val, myarr)),
+		    std::out_of_range);
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::set_adc<wordtype_t, nrows, ncols, bits_per_adc>(nrows, 0, good_adc_val, myarr)),
+		    std::out_of_range);
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::set_adc<wordtype_t, nrows, ncols, bits_per_adc>(0, -1, good_adc_val, myarr)),
+		    std::out_of_range);
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::set_adc<wordtype_t, nrows, ncols, bits_per_adc>(0, bad_adc_index, good_adc_val, myarr)),
+		    std::out_of_range);
+
+  BOOST_CHECK_THROW((dunedaq::fddetdataformats::set_adc<wordtype_t, nrows, ncols, bits_per_adc>(0, 0, bad_adc_val, myarr)),
+		    std::out_of_range);
 
 }
 
